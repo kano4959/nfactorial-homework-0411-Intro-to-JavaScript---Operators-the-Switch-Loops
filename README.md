7f211936cbe289f42497f932c0e1b361aec02521
kano4959 <kana131@yandex.ru>
2022-11-09 00:14:28 -0800

    homework3

---
+++ README.md
@@ -0,0 +1,1 @@
+# nfactorial-homework-0411-Intro-to-JavaScript---Operators-the-Switch-Loops
---
+++ homework03.js
@@ -0,0 +1,61 @@
+'use strick';
+
+
+//alert / confirm / prompt..
+
+// alert('USER');
+
+// let USER = {
+//     name: confirm('Kanat'),
+//     age: confirm('37'),
+//     gender: prompt('Men or Women?', 'Men')
+// }
+
+// console.log(USER);
+
+
+// Работа с операторами.
+
+// let d = +prompt('d');
+
+// if (d >= 89) {
+//     alert( 25 );
+// }
+// else{
+//     console.log('THE END')
+// }
+
+
+// Работа с условиями.
+
+// let a = +prompt('a?', '');
+
+// switch(a){
+//     case 0:
+//         alert ( 0 );
+//         break;
+//     case 1:
+//         alert ( 1 );
+//         break;
+//     case 2 || 3:
+//         alert( '2,3');
+// }
+
+
+// Работа с циклами(loops).
+
+// 1.
+
+// for (let q = 1; q <= 100; q++){
+//          if ( q % 2 == 0){
+//             console.log(q)}     
+// }
+
+// 2. 
+
+let i = 0;
+   while(i < 3) {
+    alert( `number ${i}!` )
+    i++; }
+
+
